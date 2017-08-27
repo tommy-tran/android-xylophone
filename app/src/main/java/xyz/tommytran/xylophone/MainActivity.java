e50ccf7321a79de38177de1663fd8b13d289c387
tommy-tran <tome7@live.com>
2017-08-27 02:33:25 -0400

    Scripting template created

--- app/src/main/java/xyz/tommytran/xylophone/MainActivity.java
+++ app/src/main/java/xyz/tommytran/xylophone/MainActivity.java
@@ -5,9 +5,35 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // TODO: Add member variables here
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Create a new SoundPool
+
+
+        // TODO: Load and get the IDs to identify the sounds
+
+
     }
+
+    // TODO: Add the play methods triggered by the buttons
 }
